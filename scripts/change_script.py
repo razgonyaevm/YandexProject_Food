c9d43133a4c645ca3805b5c74251f37d6374b8f0
Maxim Razgonyaev <razgonyaevm@gmail.com>
2021-11-17 21:04:35 +0500

    Add change script

---
+++ scripts/change_script.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QMessageBox
+from design_python.change_python import Ui_Change_database
+
+
+class Change(QMainWindow, Ui_Change_database):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.con = sqlite3.connect('../other_files/Pirgoroy.db')
+        self.update()
+
+        self.show()
+        self.exit_button.clicked.connect(self.ex)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.save_button.clicked.connect(self.save)
+
+    def ex(self):
+        self.close()
+
+    def save(self):
+        valid = QMessageBox.question(
+            self, '', "Дейтвительно сохранить?",
+            QMessageBox.Yes, QMessageBox.No)
+        if valid == QMessageBox.Yes:
+            if self.modified:
+                cur = self.con.cursor()
+                for i in self.modified.keys():
+                    que = "UPDATE Recipy SET\n"
+                    que += f"'{i[0]}'" + '=' + self.modified[i]
+                    que += f'\nWHERE id='
+                    que += str(i[1])
+                    cur.execute(que)
+                    self.con.commit()
+                self.modified.clear()
+                self.update()
+
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()], item.row() + 1] = item.text()
+
+    def update(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM Recipy").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
